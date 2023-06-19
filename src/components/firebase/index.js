9e4adc759877d42655ef09c4071348219d948c89
SohnEugene <102849934+SohnEugene@users.noreply.github.com>
2023-06-19 11:09:27 +0900

    생성

--- src/components/firebase/index.js
+++ src/components/firebase/index.js
@@ -1,18 +1,18 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqg3MweI0LFD4Oomq-oCV8dPf5cNN2JS0",
-  authDomain: "musiclog-8ac3e.firebaseapp.com",
-  projectId: "musiclog-8ac3e",
-  storageBucket: "musiclog-8ac3e.appspot.com",
-  messagingSenderId: "532635667537",
-  appId: "1:532635667537:web:3b868b67b22bfb93253c70",
-  measurementId: "G-VD0VCTVLZG",
+  apiKey: "AIzaSyBy9PR0hrlZBfpb8nboqVsghFE_CoGsscM",
+  authDomain: "music-log-3eef4.firebaseapp.com",
+  projectId: "music-log-3eef4",
+  storageBucket: "music-log-3eef4.appspot.com",
+  messagingSenderId: "305710813007",
+  appId: "1:305710813007:web:f9a2907b6ee9aad1f1bf3a",
+  measurementId: "G-VBL31CZ9F1"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const auth = getAuth(app);
 
